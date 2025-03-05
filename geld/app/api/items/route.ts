898282d8e5491df8f0aa5dad59db93d9d099c9a5
TheOneAndOnlyZestro <theoneandonlyzestro@gmail.com>
2025-03-05 22:40:24 +0100

    FIXED ERROS, now the frontend cn insert into database

--- geld/app/api/items/route.ts
+++ geld/app/api/items/route.ts
@@ -28,9 +28,15 @@ export async function GET() {
     });
   }
 }
-
+async function insert(obj: item) {
+  await query(`INSERT INTO items (name, category, price, store, date)
+    VALUES ( '${obj.name}', '${obj.category}', ${obj.price}, '${obj.store}', '${obj.date}')`);
+}
 export async function POST(request: Request) {
   const res = await request.json();
+  const body: item = res.data;
+  await insert(body);
+
   return new NextResponse(JSON.stringify({ returned: res }), {
     status: 201,
     headers: {
